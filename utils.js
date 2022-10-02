6961f3fd228a442db65a1a1c23cde40e82407579
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-10-01 18:48:04 -0800

    deconstructered answer objects

--- utils.js
+++ utils.js
@@ -1,7 +1,18 @@
-const generateHTML = (answers) => {
-    console.log(answers) 
+const generateHTML = ({title, description, installation, usage, license, Git, email, contribution}) => {
+    console.log(title);
+    console.log(description);
+    console.log(installation); 
 }
 
 module.exports = {
     generateHTML,
 };
+
+//   title: 'sfdkj',
+//   description: 'dfsdfs',
+//   installation: 'fsdf',
+//   usage: 'fsd',
+//   license: 'The MIT License',
+//   Git: 'fsdfs',
+//   email: 'sdfsdf',
+//   contribution: 'sfdsd'
